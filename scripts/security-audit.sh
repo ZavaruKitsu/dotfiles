75bebfaec62d43612a2477b3222cb136dfeb1b1c
Alexey <110727638+ZavaruKitsu@users.noreply.github.com>
2022-08-11 01:24:03 +0300

    Create security-audit.sh

---
+++ scripts/security-audit.sh
@@ -0,0 +1,11 @@
+# Rootkits and backdoors
+## rkhunter
+sudo rkhunter --update
+sudo rkhunter --check --sk --rwo
+
+# Permissions and suggestions
+## lynis
+sudo lynis audit system
+
+# Packages audit
+arch-audit
